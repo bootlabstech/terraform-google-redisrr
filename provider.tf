ff95dfbc7df37c0997abae09f30d389f8c9f0d57
Silpashree <silpashree@bootlabstech.com>
2022-12-22 15:56:36 +0530

    terraform version

--- provider.tf
+++ provider.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">=0.13"
+  required_version = "<=1.3.6"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.47.0"
+      version = "4.46.0"
     }
   }
 }
